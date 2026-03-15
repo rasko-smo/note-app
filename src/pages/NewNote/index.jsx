affa1059f76fd3032b07d6d5c99e6bdfd86111d3
rasko-smo <smo230918@gmail.com>
2026-03-15 11:24:26 +0900

    add feature to create new note

--- src/pages/NewNote/index.jsx
+++ src/pages/NewNote/index.jsx
@@ -1,17 +1,37 @@
 import { Link } from 'react-router-dom';
 import './index.css';
 import { ArrowLeft, Save } from 'lucide-react';
+import { useState } from 'react';
 
 function NewNote() {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const createNote = async () => {
+    const result = await fetch('http://localhost:3001/notes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title, content }),
+    });
+
+    if (!result.ok)
+      throw new Error(`メモの作成に失敗しました: ${result.status}`);
+
+    const data = await result.json();
+    console.log('data', data);
+  };
+
   return (
     <div className="new-note">
       <div className="new-note__header">
-        <Link className="new-note__back-btn">
+        <Link className="new-note__back-btn" to="/">
           <ArrowLeft className="new-note__back-icon" /> 戻る
         </Link>
 
         <div className="new-note__actions">
-          <button className="new-note__save-btn">
+          <button className="new-note__save-btn" onClick={createNote}>
             <Save className="new-note__save-icon" />
             保存
           </button>
@@ -25,6 +45,8 @@ function NewNote() {
               type="text"
               placeholder="タイトルを入力..."
               className="new-note__title-input"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
             />
           </div>
         </div>
@@ -33,6 +55,8 @@ function NewNote() {
           <textarea
             placeholder="メモの内容を入力してください..."
             className="new-note__textarea"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
           />
         </div>
       </div>
